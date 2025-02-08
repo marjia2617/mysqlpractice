31e3ba43f0072166c51e7068ad1e935cd5e423b6
marjia2617 <139189475+marjia2617@users.noreply.github.com>
2025-02-08 12:50:03 -0500

    Add files via upload

---
+++ Library System.sql
@@ -0,0 +1,10 @@
+create table books(
+   book_id int,
+   title varchar(25),
+   author varchar(25)
+);
+
+insert into books
+values(1, "The Alchemist", "Paulo Coelho"),
+      (2, "1984", "George Orwell"),
+      (3, "Sapiens", "Yuval Harari");
