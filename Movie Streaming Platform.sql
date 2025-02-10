13e15533365022173f3ee7f998803801f09f9579
marjia2617 <139189475+marjia2617@users.noreply.github.com>
2025-02-10 12:13:07 -0500

    Add files via upload

---
+++ Movie Streaming Platform.sql
@@ -0,0 +1,10 @@
+create table users(
+   user_id int,
+   name varchar(20),
+   email varchar(25)
+);
+
+insert into users
+values(1, "Alice", "alice@email.com"),
+      (2, "Bob", "bob@email.com"),
+      (3, "Ryan", "ryan@email.com");
