5c5cdc78a4e0215d36e02ffb9fd069f495227b32
marjia2617 <139189475+marjia2617@users.noreply.github.com>
2025-02-09 20:42:08 -0500

    Add files via upload

---
+++ Airline Reservation System.sql
@@ -0,0 +1,10 @@
+create table passengers(
+   passenger_id int,
+   name varchar(20),
+   email varchar(25)
+);
+
+insert into passengers
+values(1, "Alice", "alice@email.com"),
+      (2, "Bob", "bob@email.com"),
+      (3, "Charlie", "charmie@email.com");
