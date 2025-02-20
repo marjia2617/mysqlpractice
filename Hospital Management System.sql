e8d19635508255d632449e505ee6358cbbd49147
marjia2617 <139189475+marjia2617@users.noreply.github.com>
2025-02-19 19:06:46 -0500

    Add files via upload

--- Hospital Management System.sql
+++ Hospital Management System.sql
@@ -1,39 +1,30 @@
-List Patients with Their Doctors and Appointment Dates
+/* A hospital keeps track of patient information, doctor assignments, and treatment records. */
 create table patients(
-   patient_id int,
-   name varchar(25),
-   age int
+  id int,
+  name varchar(10),
+  age int,
+  disease varchar(15),
+  admitted_date date,
+  doctor_assigned varchar(15),
+  city varchar(15)
 );
 
 insert into patients
-values(1, "John", 45),
-      (2, "Emma", 30),
-      (3, "Ryan", 55);
-
-create table doctors(
-   doctor_id int,
-   name varchar(25),
-   specialization varchar(25)
-);
-
-insert into doctors
-values(101, "Dr.Smith", "Cardiologist"),
-      (102, "Dr.Brown", "Neurologist");
-
-create table appointments(
-   apppointment_id int,
-   patient_id int,
-   doctor_id int,
-   appointment_date date
-);
-
-insert into appointments
-values(1, 1, 101, "2024-02-01"),
-      (2, 2, 102, "2024-02-05"),
-      (3, 3, 101, "2024-02-10");
+values(1, "John Doe", 45, "Diabetes", "2024-02-10", "Dr.Smith", "New York"),
+      (2, "Alice", 30, "Flu", "2024-02-12", "Dr.Brown", "Chicago"),
+      (3, "Mark", 55, "Hypertension", "2024-01-20", "Dr.Smith", "Los Angeles"),
+      (4, "Sophia", 27, "Covid-19", "2024-02-15", "Dr. Johnson", "San Diego"),
+      (5, "Liam", 40, "Flu", "2024-02-18", "Dr.Brown", "New York");
       
-select patients.name as Patient_Name,  doctors.name as doctor_name, appointments.appointment_date
-from  appointments
-join patients on patients.patient_id = appointments.patient_id
-join doctors on doctors.doctor_id = appointments.doctor_id;
-   
+/* Find patients admitted for Flu and assigned to Dr. Brown. */
+select * from patients
+where disease = "flu" and doctor_assigned = "Dr.Brown";
+
+/* Find patients who are either above 50 years old or suffering from Covid-19. */
+select * from patients
+where age > 50 or disease = "covid-19";
+
+/* Find patients who are not admitted in New York. */
+select * from patients
+where not city = "New York";
+
