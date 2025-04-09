e805fdab684828f428983166dd635cded99eebbc
marjia2617 <139189475+marjia2617@users.noreply.github.com>
2025-04-08 21:51:13 -0400

    Job Portal - Applicants and Employees.sql

--- Job Portal - Applicants and Employees.sql
+++ Job Portal - Applicants and Employees.sql
@@ -10,6 +10,15 @@ insert into applicants
 values("Aisha", "aisha@mail.com"),
       ("Brain", "brain@mail.com"),
       ("Carla", "carla@mail.com");
+
+create table employee_10(
+   name varchar(10),
+   email varchar(20)
+);
+
+insert into employee_10
+values("Carla", "carla@mail.com"),
+      ("David", "david@company.com");
       
 /*   List everyone who interacted with the company 
     (including duplicates):    */
