283c29d82f11c8e40724bc0e5a4d6bdad82cbbbc
marjia2617 <139189475+marjia2617@users.noreply.github.com>
2025-04-08 21:23:34 -0400

    E-Commerce Orders and Returns.sql

--- E-Commerce Orders and Returns.sql
+++ E-Commerce Orders and Returns.sql
@@ -12,7 +12,17 @@ insert into order_12
 values(1, 101, "2024-01-01", 100),
       (2, 102, "2024-01-03", 200),
       (3, 101, "2024-02-01", 150);
-      
+
+create table returns(
+  return_id int,
+  customer_id int,
+  return_date date,
+  amount int
+);
+
+insert into returns
+values(1, 101, "2024-01-10", 50),
+      (2, 103, "2024-02-02", 100);  
 /*   List all transactions (orders + returns) with customer ID,
  date, amount, and type.   */
  
