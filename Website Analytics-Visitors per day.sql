8936eafbc04bb830d27dfaf720731101a8182359
marjia2617 <139189475+marjia2617@users.noreply.github.com>
2025-04-17 12:49:50 -0400

    Add files via upload

---
+++ Website Analytics-Visitors per day.sql
@@ -0,0 +1,16 @@
+/* Website analytics - Visitors Per Day */
+
+create table web_visits(
+   visit_id int,
+   user_id int,
+   visit_date date
+);
+
+insert into web_visits
+values(1, 101, "2025-04-01"),
+      (2, 102, "2025-04-01"),
+      (3, 103, "2025-04-01"),
+      (4, 104, "2025-04-02"),
+      (5, 105, "2025-04-02");
+      
+      
